d81240ad4c3e6e307ecf1d985a0d0e1a1de87859
m254104 <m254104@usna.edu>
2022-10-19 09:56:13 -0400

    classwork

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         #screen settings
-        self.screen_width = 750
-        self.screen_height = 500
+        self.screen_width = 1050
+        self.screen_height = 700
         self.bg_color = (230,230,230)
