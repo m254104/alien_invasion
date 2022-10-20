c952a46b868f548928fda3409178792a9cd0f980
m254104 <m254104@usna.edu>
2022-10-20 09:39:16 -0400

    fix

--- game_character.py
+++ game_character.py
@@ -2,10 +2,18 @@ import pygame
 import time
 
 pygame.init()
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((150,150))
 screen.fill((170, 240,240))
 
 #draw the character
-character = pygame.image.load('images\lil_ship.png')
+character = pygame.image.load('images/lil_ship.png')
+
+character_rect = character.get_rect()
+screen_rect = screen.get_rect()
+character_rect.center = screen_rect.center #move the plane to the middle
+
+#draw ('blit') the character graphic onto the screen.
+screen.blit(character, character_rect)
+
 pygame.display.flip()
 time.sleep(4)
