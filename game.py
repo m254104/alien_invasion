d7f09f7f56221d4849e18639826357888deda288
m254104 <m254104@usna.edu>
2022-10-20 08:14:53 -0400

    _check_events() method

--- game.py
+++ game.py
@@ -25,6 +25,7 @@ class AlienInvasion:
     def run_game(self):
        """Start the main loop for the game."""
        while True:
+           self._check_events()
            # watch for keyboard and mouse events
            for event in pygame.event.get():
                if event.type==pygame.QUIT:
@@ -36,6 +37,12 @@ class AlienInvasion:
 
             #make the most recently drawn screen visible
            pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 if __name__ == '__main__':
     #make a game instance, and run the game.
