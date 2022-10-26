985415a996c1ab43cf0f2e042642b585aa5c91c2
m254104 <m254104@usna.edu>
2022-10-26 09:24:59 -0400

    creating the bullet class

--- bullet.py
+++ bullet.py
@@ -17,3 +17,14 @@ class Bullet(Sprite):
 
         #store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet up the screen."""
+        #update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        #update the rect position
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
