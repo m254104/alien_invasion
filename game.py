8fdf254b58e6240229ffeacbc17687f21702c0ff
m254104 <m254104@usna.edu>
2022-10-26 09:39:57 -0400

    deleting old bullets

--- game.py
+++ game.py
@@ -32,6 +32,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            #get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+
             self._update_screen()
 
     def _check_events(self):
