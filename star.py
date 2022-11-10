eb9825f357a1e897113a2a696c0e4175a9a7ef70
m254104 <m254104@usna.edu>
2022-11-10 09:37:03 -0500

    trying to figure out hw13

---
+++ star.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+star = pygame.image.load('images/laserYellow_burst.png')
+screen_rect = screen.get_rect()
+star_rect = star.get_rect()
+
+space = screen_rect.width // star_rect.width
+
+def draw_stars():
+    for y in range(space):
+        for x in range(space):
+            screen.blit(star, (x * star_rect.width, y * star_rect.height))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    draw_stars()
